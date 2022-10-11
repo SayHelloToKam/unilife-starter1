aed1fc737f63967b3a6716e57b7cc84098a8989f
SayHelloToKam <sayhello@kamilwatson.com>
2022-10-10 22:39:56 -0400

    top of feature completed

--- src/components/FeatureBox.js
+++ src/components/FeatureBox.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import '../styles/featureBox.css'
+import { TbListSearch } from "react-icons/tb";
+import { MdChecklistRtl } from "react-icons/md";
+import { RiFilePaper2Line } from "react-icons/ri";
 
 function FeatureBox() {
   return (
-    <div>FeatureBox</div>
+    <div className='feature-container'>
+      <div className='light-blue-container'>
+        <h2>Compare all inclusive student homes.</h2>
+        <div className='feature-items'>
+          <div className='feature'>
+            <p className='feature-icon'><TbListSearch/></p>
+            <h3>Search</h3>
+            <p>Find your dream home in the perfect area near your university.</p>
+          </div>
+          <div className='feature'>
+            <p className='feature-icon'><MdChecklistRtl/></p>
+            <h3>Compare</h3>
+            <p>Compare student accommodation to find the right home for you.</p>
+          </div>
+          <div className='feature'>
+            <p className='feature-icon'><RiFilePaper2Line/></p>
+            <h3>Bills Included</h3>
+            <p>Bills are included in all rent prices. No hidden fees.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import '../styles/footer.css'
+import { FaFacebook, FaInstagram, FaTwitterSquare } from "react-icons/fa"
 
 function Footer() {
   return (
     <>
       <div className='footer-top-container'>
-      <div className='footer-contact-right'>
+      <div className='footer-contact-left'>
         <h2>Keep in touch</h2>
         <p>Curious about new offerings? Sign up for our weekly newsletter and stay informed.</p>
         <input type="email" placeholder="Your email" />
       </div>
-      <div className='footer-social-left'>
+      <div className='footer-social-right'>
         <h2>Let's Socialize</h2>
-        <p>Facebook</p>
-        <p>Twitter</p>
-        <p>Instagram</p>
+        <p><FaFacebook/>   Facebook</p>
+        <p><FaTwitterSquare/>    Twitter</p>
+        <p><FaInstagram/>   Instagram</p>
       </div>
     </div>
     <div className='footer-bottom-container'>
